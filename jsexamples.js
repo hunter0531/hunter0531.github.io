1fd7e8947a084554180c445b1d0300ae997ef511
hunter0531 <huntershore@icloud.com>
2021-07-09 13:57:46 -0600

    initial commit

---
+++ jsexamples.js
@@ -0,0 +1,6 @@
+function changeClass(){
+    document.getElementById("changeClass").className = "aqua";
+}
+function changeText(){
+    document.getElementById("changeClass").innerHTML = "You changed this text";
+}
